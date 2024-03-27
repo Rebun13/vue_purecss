35c2c70aa858807861f2205438ef59dd43c0c7b2
Rebun <rubengdopazo@gmail.com>
2024-03-27 19:46:54 +0000

    Created component for the title New component that will start all views and further styling. Ruben Gomez Dopazo

---
+++ src/components/ViewTitle.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps([
+    'title',
+    'subtitle',
+])
+</script>
+<template>
+    <div class="header">
+        <h1 class="view-title">{{ title }}</h1>
+        <h2 class="view-subtitle">{{ subtitle }}</h2>
+    </div>
+</template>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,7 +1,8 @@
 <script setup>
-
+import ViewTitle from "../components/ViewTitle.vue"
 </script>
 
 <template>
-    <h1>Main Page</h1>
+    <ViewTitle title="Hey, thank you for coming!" subtitle="Let me introduce myself" />
+    Testing the font blablabla blabla bla.
 </template>
